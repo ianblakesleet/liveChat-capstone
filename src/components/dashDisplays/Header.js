759c51c50e23086376f646220780e01841cf75b2
ianblakesleet <ianblakesleet@gmail.com>
2022-06-30 18:00:38 -0400

    added logo

--- src/components/dashDisplays/Header.js
+++ src/components/dashDisplays/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from './Header.module.css'
 import Navbar from './Navbar'
+import logo from '../../logo.png'
 
 const Header = () => {
 	return (
 		<header>
 			<div className={styles.logo}>
-				<img src="../../logo.png" alt="" />
+				<img src={logo} alt="" />
 			</div>
 			<Navbar />
 		</header>
