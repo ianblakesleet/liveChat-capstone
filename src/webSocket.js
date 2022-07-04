07b6e2ea764b039b43a1cca96df07dfe1399998e
ianblakesleet <ianblakesleet@gmail.com>
2022-07-04 15:40:10 -0400

    attempt 4

--- src/webSocket.js
+++ src/webSocket.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client'
-const socket = io.connect('http://localhost:3001')
+// const socket = io.connect('http://localhost:3001')
+const socket = io.connect('https://chatroom-capstone.herokuapp.com/')
 
 //this autoconnects, maybe move this to dashboard level in order to require authentication for login???
 
