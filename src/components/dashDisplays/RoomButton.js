1e570d8fa86101c1012978216b251c7809ea6533
ianblakesleet <ianblakesleet@gmail.com>
2022-07-05 18:48:50 -0400

    save

--- src/components/dashDisplays/RoomButton.js
+++ src/components/dashDisplays/RoomButton.js
@@ -35,7 +35,7 @@ const RoomButton = ({ room, room_id, room_author_id }) => {
 		if (room_id == roomNumber) {
 			setIsActive(true)
 		}
-	}, [])
+	})
 
 	useEffect(() => {
 		socket.emit('join_room', roomNumber)
